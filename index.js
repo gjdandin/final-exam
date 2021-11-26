c325a62fc8e78dcb39a577b15802548a19bdf6ab
mbhira7 <54266598+mbhira7@users.noreply.github.com>
2021-11-26 15:57:21 +0100

    js change

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+const mySidenav = document.getElementById("mySidenav");
+const wrapper = document.getElementById("wrapper");
+
 function openNav() {
-    const screenwidth = document.body.clientWidth;
-
-    if (screenwidth < 415) {
-        document.getElementById("mySidenav").style.width = screenwidth.toString()+"px";
-        document.getElementById("main").style.marginLeft = screenwidth.toString()+"px";
-    }
-    else {
-        document.getElementById("mySidenav").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
-    }
+  const screenwidth = document.body.clientWidth;
+  if (screenwidth < 415) {
+    mySidenav.style.width = screenwidth.toString()+"px";
+    wrapper.style.marginLeft = screenwidth.toString()+"px";
   }
-
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+    
+  else {
+    mySidenav.style.width = "250px";
+    wrapper.style.marginLeft = "250px";
   }
+}
+
+function closeNav() {
+  mySidenav.style.width = "0";
+  wrapper.style.marginLeft= "0";
+}
